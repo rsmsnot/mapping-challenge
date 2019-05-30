e3607aec317aebbc311d312e4f446097562689ff
rsmsnot <rsmsnot@outlook.com>
2019-05-30 16:10:41 +0000

    callback arguments

---
+++ callback-arguments.js
@@ -0,0 +1,17 @@
+// The second argument/parameter is expected to be a function
+var index = "";
+
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      index += i;
+      found();   // execute callback
+    }
+  }
+}
+
+function actionWhenFound() {
+  console.log("Found Waldo at index " + index + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
