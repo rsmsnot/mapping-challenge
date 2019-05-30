c7a8f67dda405b08911ce8d6877878273f9f297a
rsmsnot <rsmsnot@outlook.com>
2019-05-30 18:26:00 +0000

    callback refactor

--- callback-arguments.js
+++ callback-arguments.js
@@ -1,17 +1,17 @@
 // The second argument/parameter is expected to be a function
-var index = "";
+
 
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      index += i;
-      found();   // execute callback
+  arr.forEach((arr, index) => {
+    // console.log(index);
+    if (arr === "Waldo") {
+      found(index);   // execute callback
     }
-  }
+  });
 }
 
-function actionWhenFound() {
+function actionWhenFound(index) {
   console.log("Found Waldo at index " + index + "!");
 }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+findWaldo(["Alice", "Bob", "Winston","Waldo"], actionWhenFound);
